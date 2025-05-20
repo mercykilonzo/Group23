d632354512f53efdb735a6914ffca66d86339599
sharonmuriithi <sharonnyaguthiimuriithi@gmail.com>
2025-05-20 14:48:36 +0300

    all changes

---
+++ Question1/Kotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+val conveyor = (conveyorBelt("scarf"))
+    println(conveyor)
+}
+fun conveyorBelt(item:String):String{
+   var dequeuedItem = ""
+   var conveyorList = mutableListOf<String>("bag","shoes")
+
+    conveyorList.add(item)
+    println(conveyorList)
+    if (conveyorList.isNotEmpty()){
+        dequeuedItem = conveyorList.removeAt(0)
+    }
+    return dequeuedItem
+
+}
