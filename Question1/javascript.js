f9408abd3d172aa39c567580a450af4ef295bffa
Mercy Mwikali <mwikalimercykilonzo.com>
2025-05-20 14:46:22 +0300

    Solution in Js

---
+++ Question1/javascript.js
@@ -0,0 +1,17 @@
+
+function conveyorBelt (item){
+    let conveyorlist = ["bag","shoes"]
+    conveyorlist.push(item)
+    console.log(conveyorlist);
+    
+
+    if (conveyorlist.length > 0){
+        dequedItem = conveyorlist.shift()
+
+    }
+    return dequedItem
+    
+};
+
+console.log(conveyorBelt("scarf"));
+
